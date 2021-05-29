93be3338fde3d113aec508339b005bf634f2dcdc
robbamyers <robbamyers@icloud.com>
2021-05-29 02:05:19 -0400

    added variables and some basic arithmetic

--- Activity5/main.cpp
+++ Activity5/main.cpp
@@ -1,6 +1,44 @@
+//Author:	        Robert Myers, rfm5594, World Campus
+//Class:      	    CMPSC 121
+//Activity:   	    05
+//File:             /Users/robertmyers/OneDrive - The Pennsylvania State University/Summer 2021/CMPSC121/Activity5.cpp
+//Purpose: 	        Integer Division
+/********************************************************************\
+* Academic Integrity Affidavit:                                      *
+*                                                                    *
+* I certify that, this program code is my work.  Others may have     *
+* assisted me with planning and concepts, but the code was written,  *
+* solely, by me. I understand that submitting code which is totally  *
+* or partially the product of other individuals is a violation of the*
+* Academic Integrity Policy and accepted ethical precepts. Falsified *
+* execution results are also results of improper activities. Such    *
+* violations may result in zero credit for the assignment, reduced   *
+* credit for the assignment, or course failure.                      *
+\********************************************************************/
+// Sources of logic assistance: None
 #include <iostream>
-
+#include <iomanip>
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // variable declaration
+    int shareCount;
+    float purchasePrice, commissionRate, salePrice,
+    purchaseCost, purchaseCommissionCost, totalPurcahseCost,
+    salesRevenue, saleCommission, saleCost, totalProfitOrLoss;
+
+    // user prompt
+    cout << "Enter number of shares purchased, purchase price of stock, commission rate, and salesprice:" << endl;
+
+    // read in values from user
+    cin >> shareCount >> purchasePrice >> commissionRate >> salePrice;
+
+    // arithmetic
+    purchaseCost = shareCount * purchasePrice;
+    purchaseCommissionCost = commissionRate * purchaseCost;
+    totalPurcahseCost = purchaseCost + purchaseCommissionCost;
+    cout << "    ************ TRANSACTION DETAILS *************" << endl;
+
+    cout << purchaseCost << endl << purchaseCommissionCost << endl << totalPurcahseCost;
+
     return 0;
 }
