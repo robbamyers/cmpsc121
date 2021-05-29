237642259ee768f4ec84cd59aeb5f4cdb11d4ffa
robbamyers <robbamyers@icloud.com>
2021-05-29 01:42:06 -0400

    Add Activity5 folder

---
+++ Activity5/cmake-build-debug/CMakeFiles/Activity5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Activity5"
+  "Activity5.pdb"
+  "CMakeFiles/Activity5.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Activity5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
