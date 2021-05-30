6b04d5734ba691bebe6e5c67216f28790d4dfdde
robbamyers <robbamyers@icloud.com>
2021-05-30 12:38:33 -0400

    Experiment03-01 template, first commit

---
+++ Experiments/Experiment03-01/cmake-build-debug/CMakeFiles/Experiment03_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Experiment03_01.dir/main.cpp.o"
+  "Experiment03_01"
+  "Experiment03_01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Experiment03_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Experiments/Experiment03-01/cmake-build-debug/CMakeFiles/Experiment03_01.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Experiment03_01.
+# This may be replaced when dependencies are built.
