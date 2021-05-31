8bbd8b26d3505eb178bea180598cfe746d23ed12
robbamyers <robbamyers@icloud.com>
2021-05-30 22:42:51 -0400

    first commit

---
+++ Experiments/Experiment04/cmake-build-debug/CMakeFiles/Experiment04.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Experiment04.dir/main.cpp.o"
+  "Experiment04"
+  "Experiment04.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Experiment04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Experiments/Experiment04/experiment04.cpp
@@ -0,0 +1,26 @@
+//Author:	        Robert Myers, rfm5594, World Campus
+//Class:      	    CMPSC 121
+//Lab:   	        04
+//File:             /Users/robertmyers/OneDrive - The Pennsylvania State University/Summer 2021/cmpsc121/Experiments/Experiment04/experiment04.cpp
+//Purpose: 	        Using if/else statements to display conditional results
+/********************************************************************\
+* Academic Integrity Affidavit:                                      *
+*                                                                    *
+* I certify that, this program code is my work.  Others may have     *
+* assisted me with planning and concepts, but the code was written,  *
+* solely, by me. I understand that submitting code which is totally  *
+* or partially the product of other individuals is a violation of the*
+* Academic Integrity Policy and accepted ethical precepts. Falsified *
+* execution results are also results of improper activities. Such    *
+* violations may result in zero credit for the assignment, reduced   *
+* credit for the assignment, or course failure.                      *
+\********************************************************************/
+// Sources of logic assistance: None
+
+#include <iostream>
+
+int main() {
+
+
+    return 0;
+}
