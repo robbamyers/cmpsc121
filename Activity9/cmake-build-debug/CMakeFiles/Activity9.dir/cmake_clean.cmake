7380cbf5275db8c50b1590fe8b5fa56e96ec281f
robbamyers <robbamyers@icloud.com>
2021-06-03 19:19:30 -0400

    first commit

---
+++ Activity9/cmake-build-debug/CMakeFiles/Activity9.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Activity9"
+  "Activity9.pdb"
+  "CMakeFiles/Activity9.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Activity9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
