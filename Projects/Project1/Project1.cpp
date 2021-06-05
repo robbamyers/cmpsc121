ce80b8399a8e5fac60fd261bfcc52d505ead30e2
robbamyers <robbamyers@icloud.com>
2021-06-05 12:47:20 -0400

    first commit for Project1

---
+++ Projects/Project1/Project1.cpp
@@ -0,0 +1,27 @@
+//Author:	        Robert Myers, rfm5594, World Campus
+//Class:      	    CMPSC 121
+//Project:   	    01
+//File:             /Users/robertmyers/OneDrive - The Pennsylvania State University/Summer 2021/cmpsc121/Projects/Project1/Project1.cpp
+//Purpose: 	        North American Wind Chill Index
+/********************************************************************\
+* Academic Integrity Affidavit:                                      *
+*                                                                    *
+* I certify that, this program code is my work.  Others may have     *
+* assisted me with planning and concepts, but the code was written,  *
+* solely, by me. I understand that submitting code which is totally  *
+* or partially the product of other individuals is a violation of the*
+* Academic Integrity Policy and accepted ethical precepts. Falsified *
+* execution results are also results of improper activities. Such    *
+* violations may result in zero credit for the assignment, reduced   *
+* credit for the assignment, or course failure.                      *
+\********************************************************************/
+// Sources of logic assistance: None
+
+#include <iostream>
+
+int main() {
+    int windSpeed, temp;
+
+
+
+}
---
+++ Projects/Project1/cmake-build-debug/CMakeFiles/Project1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project1.dir/Project1.cpp.o"
+  "Project1"
+  "Project1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
