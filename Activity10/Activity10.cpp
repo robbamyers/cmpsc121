218e7543aaf628091fd759793c6ee4dd48068e36
robbamyers <robbamyers@icloud.com>
2021-06-07 17:45:06 -0400

    first commit for Activity 10

---
+++ Activity10/Activity10.cpp
@@ -0,0 +1,26 @@
+//Author:	        Robert Myers, rfm5594, World Campus
+//Class:      	    CMPSC 121
+//Activity:   	    10
+//File:             /Users/robertmyers/OneDrive - The Pennsylvania State University/Summer 2021/CMPSC121/Activity10/Activity10.cpp
+//Purpose: 	        Nested loop practice
+/********************************************************************\
+* Academic Integrity Affidavit:                                      *
+*                                                                    *
+* I certify that, this program code is my work.  Others may have     *
+* assisted me with planning and concepts, but the code was written,  *
+* solely, by me. I understand that submitting code which is totally  *
+* or partially the product of other individuals is a violation of the*
+* Academic Integrity Policy and accepted ethical precepts. Falsified *
+* execution results are also results of improper activities. Such    *
+* violations may result in zero credit for the assignment, reduced   *
+* credit for the assignment, or course failure.                      *
+\********************************************************************/
+// Sources of logic assistance: None
+
+
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
---
+++ Activity10/cmake-build-debug/CMakeFiles/Activity10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Activity10"
+  "Activity10.pdb"
+  "CMakeFiles/Activity10.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Activity10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
