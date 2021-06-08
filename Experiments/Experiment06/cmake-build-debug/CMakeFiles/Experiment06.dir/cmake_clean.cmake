c038ff68d8ba0d41bcf4e359ac289e5db75ef306
robbamyers <robbamyers@icloud.com>
2021-06-07 21:35:45 -0400

    first commit for Experiment06

---
+++ Experiments/Experiment06/cmake-build-debug/CMakeFiles/Experiment06.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Experiment06.dir/main.cpp.o"
+  "Experiment06"
+  "Experiment06.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Experiment06.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
