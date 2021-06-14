108195ef5c236dfdb7a4177329e6c8c807917988
robbamyers <robbamyers@icloud.com>
2021-06-14 17:56:46 -0400

    first commit of Activity 11

---
+++ Activity11/cmake-build-debug/CMakeFiles/Activity11.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Activity11"
+  "Activity11.pdb"
+  "CMakeFiles/Activity11.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Activity11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Activity11/cmake-build-debug/CMakeFiles/Activity11.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Activity11.
+# This may be replaced when dependencies are built.
