2a6c6c7a687c1758ca7753c8c324b0d7e46e1196
robbamyers <robbamyers@icloud.com>
2021-06-14 20:34:41 -0400

    first commit/author statement/sample text file

---
+++ Experiments/Experiment07/cmake-build-debug/CMakeFiles/Experiment07.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Experiment07.dir/main.cpp.o"
+  "Experiment07"
+  "Experiment07.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Experiment07.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
