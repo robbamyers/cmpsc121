75a49441e500024f61746f0afb163a232f6c6df1
robbamyers <robbamyers@icloud.com>
2021-06-28 11:29:08 -0400

    initial calcBalance function, working on refactor

--- Activity16/Activity16.cpp
+++ Activity16/Activity16.cpp
@@ -17,10 +17,41 @@
 \********************************************************************/
 // Sources of logic assistance: None
 #include <iostream>
+using namespace std;
+
+int calcBalance(int tx);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char continuation;
+    do{
+        double tx = 0;
+        cout << "Enter your transaction. Positive for deposit. Negative for withdrawals" << endl;
+        cin >> tx;
+        calcBalance(tx);
+
+        cout << "Do you have more transactions to enter? y/n" << endl;
+        cin >> continuation;
+    }while(toupper(continuation) == 'Y');
     return 0;
 }
+
+int calcBalance(int tx){
+    static int currentBalance = 0;
+    currentBalance += tx;
+    if(currentBalance >= 0){
+        cout << "Current balance = $" << currentBalance << endl;
+        return 0;
+    }
+    else {
+        cout << "You've overdraw your account" << endl;
+        return 1;
+    }
+}
+
+
+// enter transaction, validate to be positive
+// ask if you want to enter additional tx
+// enter transaction, calculate balance, if negative force addtl tx and require it to be positive, continue until currentBalance is positive
+// ask if you want to enter additional tx
 
 
