1d32bcc6476b8147581009bbb7611c3187565965
robbamyers <robbamyers@icloud.com>
2021-06-28 17:53:34 -0400

    first commit for lab 09

---
+++ Experiments/Experiment09/cmake-build-debug/CMakeFiles/Experiment09.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Experiment09.dir/main.cpp.o"
+  "Experiment09"
+  "Experiment09.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Experiment09.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
