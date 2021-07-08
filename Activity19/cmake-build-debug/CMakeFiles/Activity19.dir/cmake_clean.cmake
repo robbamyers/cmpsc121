77f17dcb1e0a3e58396a257bead4590ba26ab348
robbamyers <robbamyers@icloud.com>
2021-07-08 11:06:36 -0400

    first commit for Activity 19

---
+++ Activity19/cmake-build-debug/CMakeFiles/Activity19.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Activity19"
+  "Activity19.pdb"
+  "CMakeFiles/Activity19.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Activity19.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
