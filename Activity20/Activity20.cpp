c515e097ae108754155a17849aa734a7ed102c1e
robbamyers <robbamyers@icloud.com>
2021-07-08 19:44:44 -0400

    final commit for activity20

---
+++ Activity20/Activity20.cpp
@@ -0,0 +1,42 @@
+//Author:	        Robert Myers, rfm5594, World Campus
+//Class:      	    CMPSC 121
+//Activity:         20
+//File:             /Users/robertmyers/OneDrive - The Pennsylvania State University/Summer 2021/cmpsc121/Activity20/Activity20.cpp
+//Purpose: 	        Practice with recursive functions
+/********************************************************************\
+* Academic Integrity Affidavit:                                      *
+*                                                                    *
+* I certify that, this program code is my work.  Others may have     *
+* assisted me with planning and concepts, but the code was written,  *
+* solely, by me. I understand that submitting code which is totally  *
+* or partially the product of other individuals is a violation of the*
+* Academic Integrity Policy and accepted ethical precepts. Falsified *
+* execution results are also results of improper activities. Such    *
+* violations may result in zero credit for the assignment, reduced   *
+* credit for the assignment, or course failure.                      *
+\********************************************************************/
+// Sources of logic assistance: None
+#include <iostream>
+using namespace std;
+// function prototype
+void reverse(string s, int i);
+
+int main() {
+    // variable declaration and read in string value
+    string strIn;
+    cout << "Enter a string" << endl;
+    getline(cin, strIn);
+    // function call to reverse string
+    reverse(strIn, strIn.length());
+    return 0;
+}
+
+void reverse(string s, int i){
+    // base case
+    if (i != 0){
+        // display the character one less than length. .length() member function counts from 1
+        cout << s[i-1];
+        // call reverse again but pass in the character prior to this run to display
+        reverse(s,i-1);
+    }
+}
---
+++ Activity20/cmake-build-debug/CMakeFiles/Activity20.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Activity20"
+  "Activity20.pdb"
+  "CMakeFiles/Activity20.dir/Activity20.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Activity20.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
