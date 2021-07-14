ab0dd91ab1b8929a2fef3047bd9d067ef046ab83
robbamyers <robbamyers@icloud.com>
2021-07-14 07:58:58 -0400

    finished program logic

--- Experiments/Experiment12/Experiment12.cpp
+++ Experiments/Experiment12/Experiment12.cpp
@@ -18,8 +18,61 @@
 // Sources of logic assistance: None
 
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+const string CAPEQUIP = "Capital Equipment";
+const string EXPEQUIP = "Expenses Equipment";
+const string SMLPARTS = "Small Parts";
+const string TOTAL = "Total Sales";
+struct SalesRecord {
+    string invoiceNumber;
+    char equipmentCode;
+    double cost;
+};
+
+void accumulate(const SalesRecord &s, double & capSales, double &eqpSales, double & prtSales);
+void writeReport(double capSales, double eqpSales, double prtSales, double total, double capPercent, double expPercent, double prtsPercent);
+
+ofstream fout("/Users/robertmyers/OneDrive - The Pennsylvania State University/Summer 2021/cmpsc121/Experiments/Experiment12/inventoryReport.txt");
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ifstream fin;
+    fin.open("/Users/robertmyers/OneDrive - The Pennsylvania State University/Summer 2021/cmpsc121/Experiments/Experiment12/sales.txt");
+    double capSales, eqpSales, prtSales, total, capPercent, expPercent, prtsPercent;
+    SalesRecord lineItem;
+    while(fin >> lineItem.invoiceNumber >> lineItem.equipmentCode >> lineItem.cost){
+        accumulate(lineItem, capSales, eqpSales, prtSales);
+    }
+    total = capSales + eqpSales + prtSales;
+    capPercent = (capSales / total) * 100;
+    expPercent = (eqpSales / total) * 100;
+    prtsPercent = (prtSales / total) * 100;
+    writeReport(capSales, eqpSales, prtSales, total, capPercent, expPercent, prtsPercent);
+
+
     return 0;
+}
+
+void accumulate(const SalesRecord &s, double &capSales, double &eqpSales, double &prtSales){
+    if(s.equipmentCode == 'A'){
+        capSales += s.cost;
+    }
+    else if(s.equipmentCode == 'B'){
+        eqpSales += s.cost;
+    }
+    else{
+        prtSales += s.cost;
+    }
+}
+void writeReport(double capSales, double eqpSales, double prtSales, double total, double capPercent, double expPercent, double prtsPercent){
+    fout << fixed << setprecision(2);
+    fout << "       S A L E S   R E P O R T" << endl;
+    fout << CAPEQUIP << setw(25 - CAPEQUIP.length()) << "$" << capSales << setw(10) << capPercent << "%" <<  endl;
+    fout << EXPEQUIP << setw(25 - EXPEQUIP.length()) << "$" << eqpSales << setw(10) << expPercent << "%" << endl;
+    fout << SMLPARTS << setw(25 - SMLPARTS.length()) << "$" << prtSales << setw(10) << prtsPercent << "%" << endl;
+    fout << setw(33) << "---------" << endl;
+    fout << TOTAL << setw(25-TOTAL.length()) << "$" << total << endl;
+
 }
