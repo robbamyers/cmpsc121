3be62aa9a92d8b4cecb8e59a55f316253d51a0c4
robbamyers <robbamyers@icloud.com>
2021-07-19 23:57:21 -0400

    final commit for Activity23

--- Activity23/Activity23.cpp
+++ Activity23/Activity23.cpp
@@ -18,8 +18,45 @@
 // Sources of logic assistance: None
 
 #include <iostream>
+#include <ctime>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    srand(time(NULL));
+    const int SIZE = 10;
+    int nums[SIZE] = {5,4,2,7,6,8,5,2,8,14};
+
+    // #1
+    for(int i = 0; i < SIZE; i++){
+        nums[i] = nums[i] * nums[i];
+    }
+
+    // #2
+    for(int i = 0; i < SIZE; i++)
+        nums[i] += rand() % 11;
+
+    // #3
+    for(int i = 0; i < (SIZE - 1); i++){
+        nums[i] += nums[i+1];
+    }
+
+    // #4
+    int sum = 0;
+    for (int i = 0; i < SIZE; i++){
+        sum +=  nums[i];
+    }
+
+    // #5
+    int largest;
+    for(int i = 0; i < (SIZE - 1); i++){
+        if(nums[i] > nums[i+1]){
+            largest = nums[i];
+        }
+    }
+    cout << largest;
     return 0;
 }
+/* Sample Execution:
+266
+Process finished with exit code 0
+*/
