56b357bd0da0f6adc0d59bffc632c3590d1c696f
robbamyers <robbamyers@icloud.com>
2021-07-20 00:00:15 -0400

    first commit for activity24

---
+++ Activity24/Activity24.cpp
@@ -0,0 +1,25 @@
+//Author:	        Robert Myers, rfm5594, World Campus
+//Class:      	    CMPSC 121
+//Activity:   	    24
+//File:             /Users/robertmyers/OneDrive - The Pennsylvania State University/Summer 2021/cmpsc121/Activity24/Activity24.cpp
+//Purpose: 	        Practice with vectors
+/********************************************************************\
+* Academic Integrity Affidavit:                                      *
+*                                                                    *
+* I certify that, this program code is my work.  Others may have     *
+* assisted me with planning and concepts, but the code was written,  *
+* solely, by me. I understand that submitting code which is totally  *
+* or partially the product of other individuals is a violation of the*
+* Academic Integrity Policy and accepted ethical precepts. Falsified *
+* execution results are also results of improper activities. Such    *
+* violations may result in zero credit for the assignment, reduced   *
+* credit for the assignment, or course failure.                      *
+\********************************************************************/
+// Sources of logic assistance: None
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    return 0;
+}
